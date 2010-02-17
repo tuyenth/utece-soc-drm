56994b0f086f4e50a788777aaf31629c67fff4dd
lev.shuhatovich <lev.shuhatovich@83ac582e-1b61-11df-8c1f-719d98f73570>
2010-02-17 02:04:25 +0000

    Initial commit of DRM flp and fxp as posted on the class webpage.

---
+++ trunk/drm-1.2.4-flp/config_linux.pl
@@ -0,0 +1,30 @@
+#!/usr/bin/perl
+
+$pwd = `pwd`;
+chomp $pwd;
+$filename = "$pwd/config_linux_soc";
+
+open (FHI, ">$filename") || die "$!\n";
+
+$cmd = "export CXX='g++ -m32 -fpermissive'";
+print FHI $cmd."\n";
+$cmd = "export CXXFLAGS='-pg -g -O2 -L$pwd/fft_lib -I$pwd/fft_inc'";
+print FHI $cmd."\n";
+
+$cmd = "./configure --without-qtgui --disable-sound --disable-alsa --disable-simulation --without-pic  --disable-dependency-tracking  --without-faac";
+print FHI $cmd."\n";
+
+system("cat $filename");
+system("chmod 755 $filename");
+
+print "==================================================================\n";
+print "DRM setup for SOC class (Minsik Cho)\n";
+print "Please follow the steps\n";
+print "1) %>./config_linux_soc\n";
+print "2) %>make\n";
+print "3) %>cd linux\n";
+print "4) %>./drm\n";
+print "5) %>cmp RTL_ModeB_10kHz_gold.wav dummy.wav\n";
+print "6) You shouldn't see any msg from 5).\n";
+print "==================================================================\n";
+
